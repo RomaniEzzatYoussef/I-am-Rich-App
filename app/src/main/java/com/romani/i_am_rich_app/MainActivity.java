92e71ca5654591651dd844c03b8122b4da6aa1a9
Romani Ezzat <romaniezzat@hotmail.com>
2019-12-05 12:10:45 +0200

    Initial commit

---
+++ app/src/main/java/com/romani/i_am_rich_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.romani.i_am_rich_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
